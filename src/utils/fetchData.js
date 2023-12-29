cc33c689ba8bd9d9e870c6c9be3e43a5c8272d43
Andrii P <andrii.petryk@jda.com>
2023-12-29 18:58:07 -0500

    added changes in to the build process

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,7 +2,7 @@ export const exerciseOptions = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Key': 'b400da4d81msh7bc7f791c43a550p19ece1jsn6034f4ac8933',
-    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
   },
 };
 
@@ -10,7 +10,7 @@ export const youtubeOptions = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Key': 'b400da4d81msh7bc7f791c43a550p19ece1jsn6034f4ac8933',
-    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
   },
 };
 
